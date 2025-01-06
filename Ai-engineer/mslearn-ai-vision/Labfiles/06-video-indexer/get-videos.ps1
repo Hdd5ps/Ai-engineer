0a5781daf762b8f1b648f8fe48736469ad55c027
Hdd5ps <hdd5ps@virginia.edu>
2025-01-06 16:09:40 +0000

    video indexer

--- Ai-engineer/mslearn-ai-vision/Labfiles/06-video-indexer/get-videos.ps1
+++ Ai-engineer/mslearn-ai-vision/Labfiles/06-video-indexer/get-videos.ps1
@@ -1,5 +1,5 @@
-$account_id="YOUR_ACCOUNT_ID"
-$api_key="YOUR_API_KEY"
+$account_id="958c3c55-0f39-41c2-9b92-ee4c45c525a1"
+$api_key="7228a77403d24a56a15e1c4c65b8b1ef"
 $location="trial"
 
 # Call the AccessToken method with the API key in the header to get an access token
